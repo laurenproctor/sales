1a3d655b5ede2477abb4bfaa2a1f4df2e470c581
laurenproctor <laurenproctor32@gmail.com>
2013-11-02 10:58:00 -0400

    creating sale model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,10 @@
 class Product < ActiveRecord::Base
+  has_many :sales
+
+  validates_numericality_of :price,
+  	greater_than: 49,
+  	message: "Must be at least 50 cents."
+
   has_attached_file :file
 
   belongs_to :user
